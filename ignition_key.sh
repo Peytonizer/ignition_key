af13a6c94421a263293b9c405b0ddf6a22c6f170
Peytonizer <matt.peyton@gmail.com>
2021-10-06 20:24:44 +1100

    Update ignition_key.sh

--- ignition_key.sh
+++ ignition_key.sh
@@ -32,6 +32,9 @@ fi
 BLUE "Updating repositories..."
 sudo apt update
 
+BLUE "Updating certs..."
+sudo apt install ca-certificates -y
+
 BLUE "Getting Java JDK..."
 sudo apt install -y default-jdk
 
@@ -54,7 +57,7 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
 sudo apt install -y apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
-sudo apt-get install -y sublime-text
+sudo apt install -y sublime-text
 
 BLUE "Installing NodeJS..."
 sudo apt-get install nodejs -y
